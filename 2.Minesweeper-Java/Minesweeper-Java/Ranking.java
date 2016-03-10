6759a5859c78748ce786c9bc6798afcb2c709467
Lana Vu <anhlv@ifi.uio.no>
2016-03-10 15:37:16 +0100

    Modified ranking.java

--- 2.Minesweeper-Java/Minesweeper-Java/Ranking.java
+++ 2.Minesweeper-Java/Minesweeper-Java/Ranking.java
@@ -14,7 +14,10 @@ public class Ranking{
 		last=0;
 	}
 
-
+/** Records the name and the result of the player 
+ * @param the result of the game play (how many steps the player survived)
+ * 
+ */
 	public void recordName(int result) {
 		System.out.print("\n Please enter your name -");
 		Scanner in=new Scanner(System.in);
@@ -24,23 +27,23 @@ public class Ranking{
 			return;
 		}
 		else if(last==MAX_PEOPLE_LIMIT){
-			name[MAX_PEOPLE_LIMIT-1]=newName;{
-				record[MAX_PEOPLE_LIMIT-1]=result;
-			}
+			name[MAX_PEOPLE_LIMIT-1]=newName;
+			record[MAX_PEOPLE_LIMIT-1]=result;
+	
 		}
 		else{
-			name[last]=newName;{
-				record[last]=result;{
-					last++;
-				}
-			}
+			name[last]=newName;
+			record[last]=result;
+			last++;				
+			
 		}
-		
 		sort();
 		show();
 	}
 
-
+/**
+ * Shows the score board 
+ */
 	public void show() {
 		if(last==0){
 			System.out.println("Still no results");
@@ -52,25 +55,35 @@ public class Ranking{
 		}
 	}
 	
-	
+/**
+ * Sorts the results by score
+ */
 	private void sort(){
 		if(last<2) return;
+		
 		boolean unsorted=true;
 		while(unsorted){
-			unsorted=false;
-			for(int i=0;i<(last-1);i++){
-				if(record[i+1]>record[i]){
-					int swapR=record[i];{
-						record[i]=record[i+1];{
-							record[i+1]=swapR;
-							String swapN=name[i];
-							name[i]=name[i+1];
-							name[i+1]=swapN;
-							unsorted=true;
-						}
-					}
-				}
+			unsorted= swap(false);
+
+		}
+	}
+	/**
+	 * Swaps elements to sort the list by scores
+	 * @param false for unsorted list
+	 * @return returns true when the list is sorted
+	 */
+	private boolean swap(boolean unsorted){
+		for(int i=0;i<(last-1);i++){
+			if(record[i+1]>record[i]){
+				int swapR=record[i];
+				record[i]=record[i+1];
+				record[i+1]=swapR;
+				String swapN=name[i];
+				name[i]=name[i+1];
+				name[i+1]=swapN;
+				unsorted = true;	
 			}
 		}
+		return unsorted;
 	}
 }
